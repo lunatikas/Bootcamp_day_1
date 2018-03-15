85e532af8201ffe2fa7a71eb43f912e766088215
Tomas Namajuska <tomasnamajuska@Tomass-MacBook-Pro.local>
2018-03-15 11:55:09 +0200

    kazkoks kosmosas

--- src/sudauginti.java
+++ src/sudauginti.java
@@ -1,7 +1,7 @@
 public class sudauginti {
     public static void main (String[] args){
         int a= 2;
-        int b= 2;
+        int b= 4;
         int sum= a*b;
         System.out.println(sum);
     }
